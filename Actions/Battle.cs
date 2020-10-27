dd7217a41da2de795a430cf5a2cfea601dbac0b6
icyDenev <44680144+icyDenev@users.noreply.github.com>
2020-10-27 09:35:11 +0200

    Add files via upload

---
+++ Actions/Battle.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: zala1
+ * Date: 10/26/2020
+ * Time: 10:38 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Cards.Actions
+{
+	/// <summary>
+	/// Description of Battle.
+	/// </summary>
+	public class Battle
+	{
+		public Battle()
+		{
+		}
+	}
+}
