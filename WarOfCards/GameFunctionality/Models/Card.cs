e8a1090791430c6460082573fdc48b001b0862df
Vesk <veskmail@gmail.com>
2020-10-27 18:37:57 +0200

    Established structure and added console functionality

---
+++ WarOfCards/GameFunctionality/Models/Card.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: zala2
+ * Date: 10/27/2020
+ * Time: 10:30 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Cards.GameFunctionality.New_Folder
+{
+	/// <summary>
+	/// Description of Card.
+	/// </summary>
+	public class Card
+	{
+		public Card()
+		{
+		}
+	}
+}
