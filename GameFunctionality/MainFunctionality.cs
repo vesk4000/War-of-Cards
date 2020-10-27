e932ae541d9df149ff0c40f8490ad67565cbb25f
icyDenev <44680144+icyDenev@users.noreply.github.com>
2020-10-27 10:50:48 +0200

    Add files via upload
    Basic game functionality

---
+++ GameFunctionality/MainFunctionality.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: zala2
+ * Date: 10/27/2020
+ * Time: 10:27 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Cards.GameFunctionality
+{
+	/// <summary>
+	/// Description of MainFunctionality.
+	/// </summary>
+	public class MainFunctionality
+	{
+		
+	}
+}
